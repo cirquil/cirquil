6e90f77f2dd34589e7087e50fa5d7a301cb9edec
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2023-12-19 23:44:37 +0700

    Remove cdm16.circ and tests for xml converter

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use crate::test_propagate::test_propagate;
-use crate::test_converter::test_converter;
 
 mod core;
 mod gui;
 mod logisim;
 mod test_propagate;
-mod test_converter;
 
 fn main() {
     test_propagate();
-    test_converter();
 }
--- src/test_converter.rs
+++
@@ -1,5 +0,0 @@
-use crate::logisim::converter::parse_logisim;
-
-pub fn test_converter() {
-    println!("{:?}", parse_logisim());
-}
