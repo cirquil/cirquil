490b19035e47029b5885c422a622b965cabfdd3c
andrey-dru-me1 <andrey260503@gmail.com>
2024-06-18 02:40:53 +0700

    Change logisim lib type from String to Option<u32>

--- src/logisim/converter/component.rs
+++ src/logisim/converter/component.rs
@@ -8,18 +8,20 @@ use crate::core::simulation::components::tunnel::Tunnel;
 use crate::logisim::parser::component::LogisimComponent;
 
 pub fn convert_logisim_component(logisim_component: &LogisimComponent) -> Component {
-    match (logisim_component.lib.as_str(), logisim_component.name.as_str()) {
-        ("0", "Clock") => ClockGenerator::create(),
-        ("5", "Button") => InputButton::create(),
-        ("1", "OR Gate") => OrGate::from_bit_width(1),
-        ("1", "AND Gate") => AndGate::from_bit_width(1),
-        ("1", "NOT Gate") => NotGate::from_bit_width(1),
-        ("0", "Tunnel") => {
-            Tunnel::from_name_width(
-                logisim_component.get_param("label").unwrap(),
-                1,
-            )
-        }
-        _ => panic!("Unknown component {} from lib {}", logisim_component.name, logisim_component.lib),
+    match (
+        logisim_component.lib.unwrap(),
+        logisim_component.name.as_str(),
+    ) {
+        (0, "Clock") => ClockGenerator::create(),
+        (5, "Button") => InputButton::create(),
+        (1, "OR Gate") => OrGate::from_bit_width(1),
+        (1, "AND Gate") => AndGate::from_bit_width(1),
+        (1, "NOT Gate") => NotGate::from_bit_width(1),
+        (0, "Tunnel") => Tunnel::from_name_width(logisim_component.get_param("label").unwrap(), 1),
+        _ => panic!(
+            "Unknown component {} from lib {}",
+            logisim_component.name,
+            logisim_component.lib.unwrap()
+        ),
     }
 }
